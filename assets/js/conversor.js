67e210be3a5d3a425e359542e63977ab4af25801
Cony <alu0100673647@ull.edu.es>
2016-03-08 21:44:23 +0000

    Añadiendo clases

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,24 +1,66 @@
 (function(exports) {
   "use strict";
-
-  function Medida(valor,tipo)  
-  {
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
+    
+    
+  function Medida(valor,tipo)  {
+    this.valor = valor || 0;
+    this.tipo = tipo || "no-definido";
+   
   }
+   
+  Medida.constructor = Medida;  
   
-  function Temperatura(valor,tipo)
-  {
+  function Temperatura(valor,tipo){
+    Medida.this(this, valor, tipo);
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
-  }
+  }// fin temperatura
+ 
+ 
+  Temperatura.prototype = new Medida(); // herencia
+  Temperatura.prototype.constructor = Temperatura;
   
-  function Celsius(valor)
-  {
-  }
+  function Celsius(valor){
+    Temperatura.call(this, valor, "C");
+    /*Funcion para pasar de celsius a farenheit*/
+    this.toFarenheit = function(){
+      return (( valor * 9/5) + 32);
+    };
+    /*Funcion para pasar de celsius a kelvin*/
+    this.toKelvin = function(){
+      return (valor + 273.15);
+    };  
+    
+  } // fin Celsius
+
+  Celsius.prototype = new Temperatura();
+  Celsius.prototype.constructor = Celsius;
   
-  function Farenheit(valor)
-  {
+  function Farenheit(valor){
+    /*Funcion para pasar de  farenheit a celsius*/
+    this.toCelsius = function(){
+      return((valor - 32) * 5/9);
+    };
+    /*Funcion para pasar de farenheit a kelvin*/
+    this.toKelvin = function(){
+      return((valor + 459.67) * 5/9); 
+    };
   }
+  Farenheit.prototype = new Temperatura();
+  Farenheit.prototype.constructor = Farenheit;
+  
+ 
+  function Kelvin(valor){
+    this.toCelsius = function(){
+      return (valor - 273.15);
+    }
+    this.toFarenheit = function(){
+      return(valor * 9/5 - 459.67);  
+    }
+  }
+  Kelvin.prototype = new Temperatura();
+  Kelvin.prototype.constructor = Kelvin;
   
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
